d9d0c6e0f2d4e32420940262e3a99d0afcb26d8e
vnporras <vnporras1028@gmail.com>
2024-02-05 19:22:36 -0800

    complete the code

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(str[39:67], end="")
-print(str[107:111], str[:6])
+print(f"{str[107:111] + ' ' + str[:6]}")
