255cfa8229d5ab43d3671df4c5101e020e518f35
vnporras <vnporras1028@gmail.com>
2024-02-07 14:34:34 -0800

    modified the function

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alpha in range(ord('a'), ord('z')+1):
     if chr(alpha) not in ['q', 'e']:
-        print(chr(alpha), end='')
+        print("{0}".format(chr(alpha)), end='')
