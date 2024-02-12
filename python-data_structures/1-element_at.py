48482f62555f2294251f08bcbd2672a6f9c9d894
vnporras <vnporras1028@gmail.com>
2024-02-11 20:24:35 -0800

    create the function

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
+#!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    for i in range(len(my_list)):
-        if i == idx:
-            return my_list[i]
+  if idx < 0 or idx > len(my_list):
     return None
+  else:
+    return my_list.pop(idx)
