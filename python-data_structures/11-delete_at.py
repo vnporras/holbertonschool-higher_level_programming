cf7ff3c654b3338e9af7c5e9f811bad74788632e
vnporras <vnporras1028@gmail.com>
2024-02-14 20:30:39 -0800

    create the function

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,13 +2,14 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 and idx >= len(my_list):
         return my_list
- 
-    result = []
-    counter = 0
 
-    while counter < len(my_list):
+    result = my_list.copy()
+    counter = 0
+    my_list.clear()
+
+    while counter < len(result):
         if counter != idx:
-            result.append(my_list[counter])
+            my_list.append(result[counter])
         counter = counter + 1
 
-    return result
+    return my_list
