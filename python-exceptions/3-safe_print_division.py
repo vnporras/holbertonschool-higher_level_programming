367a7f84c7bb49265c10bd77a040349cb4b2ad74
vnporras <vnporras1028@gmail.com>
2024-02-17 21:16:34 -0800

    modified

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
         return result
-    except:
+    except Exception:
         result = None
     finally:
         print("Inside result:", end=" ")
