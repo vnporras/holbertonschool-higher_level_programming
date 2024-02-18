9fa6151b28e9846287290cd612477fc03677dbbe
vnporras <vnporras1028@gmail.com>
2024-02-17 21:11:43 -0800

    modified

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,11 +3,12 @@
 def safe_print_list(my_list=[], x=0):
     counter = 0
     try:
-        for index, value in enumerate(my_list):
-            if (index < x):
-                print(str(value), end="")
+        for i in range(0, x):
+            if (i < x):
+                print(str(my_list[i]), end="")
                 counter += 1
         print()
         return counter
     except Exception:
+        print()
         return counter
