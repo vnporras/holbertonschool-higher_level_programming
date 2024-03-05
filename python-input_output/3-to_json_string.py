b02a2de2211b5ce72774f459fdb3f74e7d20f9ee
vnporras <vnporras1028@gmail.com>
2024-03-05 19:18:24 +0000

    modified the documentation

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+"""Class to JSON"""
 import json
-"""to JSON string"""
 def to_json_string(my_obj):
     """returns the JSON representation of an object (string)"""
     return json.dumps(my_obj)
