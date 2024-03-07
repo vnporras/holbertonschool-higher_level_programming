bba874f5d39b9111878d2792338c8cc1efe0de46
vnporras <vnporras1028@gmail.com>
2024-03-07 16:47:01 +0000

    documentation in classes and modules

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
+"""Rectangle class that inherits from Base"""
 from base import Base
 
 class Rectangle(Base):
+    """Rectangle class that inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """constructor of the class Rectangle"""
         self.__width = width
         self.__height = height
         self.__x = x
@@ -12,32 +14,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter of the width attribute"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter of the width attribute"""
         self.__width = value
 
     @property
     def height(self):
+        """getter of the height attribute"""
         return self.__height
     
     @height.setter
     def height(self, value):
+        """setter of the height attribute"""
         self.__height = value
 
     @property
     def x(self):
+        """getter of the x attribute"""
         return self.__x
     
     @x.setter
     def x(self, value):
+        """setter of the x attribute"""
         self.__x = value
 
     @property
     def y(self):
+        """getter of the y attribute"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """setter of the y attribute"""
         self.__y = value
