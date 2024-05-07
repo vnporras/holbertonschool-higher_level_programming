e3d9ba51fae776b7cdeae03af4572ad9772f4ff4
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-05-07 15:10:36 -0500

    modified the style

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,3 @@ for i in range(0, 100):
     if (i < 10):
         print('0', end='')
     print("{}{}".format(i, ', ' if i != 99 else '\n'), end='')
-    
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,3 @@ for num1 in range(10):
             print('{}{}'.format(num1, num2))
         else:
             print('{}{}, '.format(num1, num2), end='')
-            
