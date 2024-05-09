ebb299b31fb37626789c74ac108d7fa2996b6d74
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-05-09 12:07:09 -0500

    modified the style

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
 
+
 def add_arguments(args):
     addition = sum(int(argument) for argument in args)
     return addition
+
 
 if __name__ == "__main__":
     arguments = argv[1:]
