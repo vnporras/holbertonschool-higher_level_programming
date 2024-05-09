40202ecde3741f66d9fe17b9d39f5bbde5e267de
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-05-09 12:03:21 -0500

    add the file Infinite addition

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+def add_arguments(args):
+    addition = sum(int(argument) for argument in args)
+    return addition
+
+if __name__ == "__main__":
+    arguments = argv[1:]
+    result = add_arguments(arguments)
+    print(result)
