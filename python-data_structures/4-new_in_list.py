9fe50dff2914d2506da183443c84698ad8025cb7
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-05-14 15:17:28 -0500

    modified the code

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     my_list[:]
-    if idx < 0 and idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         return my_list
     else:
         cp_list = my_list[:]
