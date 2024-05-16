ab64f59d998ff492ba9a129f3b4b5c865a228817
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-05-16 11:26:33 -0500

    modified the code

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -23,8 +23,3 @@ def common_elements(set_1, set_2):
             resultList.append(i)
 
     return resultList
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
