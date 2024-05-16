b841202a4dc8147b1d732392e02115e397dc967c
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-05-16 14:55:33 -0500

    modified the code

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -23,8 +23,3 @@ def only_diff_elements(set_1, set_2):
             resultList.append(i)
 
     return resultList
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-od_set = only_diff_elements(set_1, set_2)
-print(sorted(list(od_set)))
