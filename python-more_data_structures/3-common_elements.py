f26de7f2f528f2ebc555ae9884da29871e13d2b8
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-05-16 11:23:55 -0500

    modified the code

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,30 +1,9 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    resultDictionary = {}
-    resultList = []
-
-    for i in set_1:
-        valueDictionary = resultDictionary.get(i)
-        if valueDictionary is None:
-            resultDictionary[i] = 1
-        else:
-            resultDictionary[i] = valueDictionary + 1
-
-    for i in set_2:
-        valueDictionary = resultDictionary.get(i)
-        if valueDictionary is None:
-            resultDictionary[i] = 1
-        else:
-            resultDictionary[i] = valueDictionary + 1
-
-    for i in resultDictionary:
-        if resultDictionary[i] >= 2:
-            resultList.append(i)
-
-    return resultList
+    return list(set_1 & set_2)
 
 set_1 = { "Python", "C", "Javascript" }
 set_2 = { "Bash", "C", "Ruby", "Perl" }
 c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
+print(sorted(c_set))
