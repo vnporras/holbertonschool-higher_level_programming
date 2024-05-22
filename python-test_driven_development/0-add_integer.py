21b9050743f2a96e8c52426cacbc6430a523bf80
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-05-22 14:26:15 -0500

    modified the code and test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -27,4 +27,8 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [float, int]:
         raise TypeError("b must be an integer")
+    if a is float('inf'):
+        raise OverflowError('cannot convert float infinity to integer')
+    if b is float('inf'):
+        raise OverflowError('cannot convert float infinity to integer')
     return int(a + b)
