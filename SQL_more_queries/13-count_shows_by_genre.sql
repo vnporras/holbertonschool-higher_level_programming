45d3042f1ca026f532022fde1506330c8a7d61f3
Vanessa Porras <vnporras1028@gmail.com>
2024-06-27 14:35:45 -0500

    add task

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,13 @@
+SELECT 
+    g.name AS genre, 
+    COUNT(tg.show_id) AS number_of_shows
+FROM 
+    tv_genres g
+JOIN 
+    tv_show_genres tg ON g.id = tg.genre_id
+GROUP BY 
+    g.name
+HAVING 
+    COUNT(tg.show_id) > 0
+ORDER BY 
+    number_of_shows DESC;
