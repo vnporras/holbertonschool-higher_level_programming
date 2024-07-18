146ffba4f0faab3eaccd4989a16985fea2c7f904
vnporras <vnporras1028@gmail.com>
2024-07-18 14:23:22 -0500

    add task 0

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,7 +1,7 @@
 def generate_invitations(template, attendees):
     import os
 
-    # Verifica tipos de entrada
+    # Check Input Types and Specific Error Handling Behaviors / Verifica tipos de entrada
     if not isinstance(template, str):
         print("Error: The template must be a string.")
         return
@@ -10,17 +10,17 @@ def generate_invitations(template, attendees):
         print("Error: The assistants should be a list of dictionaries")
         return
 
-    # Maneja plantilla vacía
+    # Specific Error Handling Behaviors, Handle Empty Inputs / Maneja plantilla vacía
     if not template.strip():
         print("Error: The template is empty, no output files were generated.")
         return
 
-    # Maneja lista vacía de asistentes
+    # Specific Error Handling Behaviors / Maneja lista vacía de asistentes
     if not attendees:
         print("Error: No data provided, no output files generated.")
         return
 
-    # Procesar cada asistente y generar archivos de salida
+    # Process Each Attendee and generate output files/ Procesar cada asistente y generar archivos de salida
     for index, attendee in enumerate(attendees, start=1):
         output_content = template
         for key, value in attendee.items():
@@ -30,7 +30,7 @@ def generate_invitations(template, attendees):
         # Reemplazar cualquier placeholder que se no sea encontrado
         output_content = output_content.format(name="N/A", event_title="N/A", event_date="N/A", event_location="N/A")
 
-        # Escribir archivo de salida
+        # Generate Output Files / Escribir archivo de salida
         output_filename = f"output_{index}.txt"
         with open(output_filename, 'w') as output_file:
             output_file.write(output_content)
